b5c9bedd4109a6e8d28ba456aa217a3b58d182c2
Hubert Krzywonos <krzywonos.h@gmail.com>
2025-01-25 14:38:09 +0100

    csv files with genre cooccurences, and links between imdb ids and genres

--- transform.py
+++ transform.py
@@ -1,16 +1,30 @@
 import csv
 import ast
+from collections import Counter
+from itertools import combinations
 
-genres = set()
+genre_cooccurrences = Counter();
+movie_genre_links = [];
+
 with open("data/movies_metadata.csv", encoding="utf-8",errors="replace") as csvfile:
     reader = csv.DictReader(csvfile);
     for row in reader:
-        row_genres = ast.literal_eval(row["genres"]);
-        for genre in row_genres:
-            genres.add((genre["id"], genre["name"]));
+        genres = ast.literal_eval(row["genres"]);
+        genre_names = [genre["name"].replace(" ", "_") for genre in genres];
+        for genre_1, genre_2 in combinations(sorted(genre_names), 2):
+            genre_cooccurrences[(genre_1, genre_2)] += 1;
+        movie_id = row["imdb_id"];
+        for genre in genre_names:
+            movie_genre_links.append((movie_id, genre))
 
-genres_list = sorted(genres, key=lambda x: x[0]);
-print(genres_list)
+with open('data/genre_cooccurrences.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['genre_1', 'genre_2', 'count'])
+    for (genre_1, genre_2), count in genre_cooccurrences.items():
+        writer.writerow([genre_1, genre_2, count])
 
-genre_names = [name for _, name in genres_list]
-print(genre_names)
+with open('data/movie_genre_links.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['movie', 'genre'])
+    for movie, genre in movie_genre_links:
+        writer.writerow([movie, genre])
