aaa997c019fc53d33337855da615ab76d50334bc
Hubert Krzywonos <krzywonos.h@gmail.com>
2025-01-26 15:46:23 +0100

    new csv with genre cooccurences and release years

--- transform.py
+++ transform.py
@@ -4,7 +4,7 @@ from collections import Counter
 from itertools import combinations
 
 genre_cooccurrences = Counter();
-movie_genre_links = [];
+genre_year_combinations = [];
 
 with open("data/movies_metadata.csv", encoding="utf-8",errors="replace") as csvfile:
     reader = csv.DictReader(csvfile);
@@ -13,18 +13,20 @@ with open("data/movies_metadata.csv", encoding="utf-8",errors="replace") as csvf
         genre_names = [genre["name"].replace(" ", "_") for genre in genres];
         for genre_1, genre_2 in combinations(sorted(genre_names), 2):
             genre_cooccurrences[(genre_1, genre_2)] += 1;
-        movie_id = row["imdb_id"];
-        for genre in genre_names:
-            movie_genre_links.append((movie_id, genre))
+        release_date = row.get('release_date', '');
+        release_year = release_date[:4] if release_date and release_date[:4].isdigit() else None;
+        if release_year:
+            for genre_1, genre_2 in combinations(sorted(genre_names), 2):
+                genre_year_combinations.append((genre_1, genre_2, release_year));
 
 with open('data/genre_cooccurrences.csv', 'w', encoding='utf-8', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['genre_1', 'genre_2', 'count'])
+    writer = csv.writer(file);
+    writer.writerow(['genre_1', 'genre_2', 'count']);
     for (genre_1, genre_2), count in genre_cooccurrences.items():
-        writer.writerow([genre_1, genre_2, count])
+        writer.writerow([genre_1, genre_2, count]);
 
-with open('data/movie_genre_links.csv', 'w', encoding='utf-8', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['movie', 'genre'])
-    for movie, genre in movie_genre_links:
-        writer.writerow([movie, genre])
+with open('data/genre_cooccurences_years.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file);
+    writer.writerow(['genre_1', 'genre_2', 'release_year']);
+    for genre_1, genre_2, year in genre_year_combinations:
+        writer.writerow([genre_1, genre_2, year]);
