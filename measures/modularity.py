5262e50812187ab23b7a071be2ec531dbbdd4b71
Liber-R <132198967+Liber-R@users.noreply.github.com>
2025-02-05 17:34:19 +0100

    Update modularity.py

--- measures/modularity.py
+++ measures/modularity.py
@@ -1,7 +1,7 @@
 import networkx as nx
 from networkx.algorithms.community import quality as nx_quality
 from networkx.algorithms.community import louvain_communities
-from util import csv_to_multigraph, MG_to_G
+from util import csv_to_multigraph, MG_to_G, decade_graphs
 import pandas as pd
 
 MG = csv_to_multigraph('../data/genre_cooccurrences_in_a_year.csv', 'weight', 'label')
@@ -26,3 +26,21 @@ for _ in louvain_comm:
 
 Q2 = nx_quality.modularity(MG, louvain_comm)
 print("\n\nModularity Q2:", Q2)
+
+d_greedy_modularity = dict()
+d_louvian_modularity = dict()
+# Same measures, but for each decade
+for decade, graph in graphs_by_decade.items():
+    d_communities = nx.community.greedy_modularity_communities(graph)
+    d_Q1 = nx_quality.modularity(graph, d_communities)
+    d_greedy_modularity[decade] = (d_communities, d_Q1)
+
+    d_louvian_comm = louvain_communities(graph)
+    d_Q2 = nx_quality.modularity(graph, d_louvian_comm)
+    d_louvian_modularity[decade] = (d_louvian_comm, d_Q2)
+
+df_greedy = pd.DataFrame(d_greedy_modularity.values(), d_greedy_modularity.keys(), columns=["Communities", "Modularity"])
+df_louvian = pd.DataFrame(d_louvian_modularity.values(), d_louvian_modularity.keys(), columns=["Communities", "Modularity"])
+
+df_greedy.to_csv("measures_data/Greedy_Modularity_by_decades.csv", ",", index_label="Decades")
+df_louvian.to_csv("measures_data/Louvian_Modularity_by_decades.csv", ",", index_label="Decades")
